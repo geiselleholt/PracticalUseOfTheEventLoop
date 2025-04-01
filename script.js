b3742cf7a291cdb2d1a74d8a5c4af1f60eb5b40c
Geiselle Holt <geiselleholt@hotmail.com>
2025-04-01 12:21:22 -0600

    Part 2 done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// PART 1 ----------------------
 let counter = 0;
 
 function increment() {
@@ -11,3 +12,38 @@ function increment() {
 }
 
 increment();
+
+// PART 2 ----------------------
+
+// function flattenArray(parentArray) {
+//   parentArray.forEach((array) => {
+//     let flatArray = array.flat();
+//     flattenArray(flatArray);
+//   });
+// }
+
+const trampoline = (f, ...args) => {
+    let result = f(...args);
+    while (typeof result === "function") {
+      result = result();
+    }
+    return result;
+  }
+
+function flattenArrayHelper(array, output = []) {
+    if (array.length === 0) {
+        return output;
+    }
+    let [first, ...rest] = array;
+    if (Array.isArray(first)) {
+        return () => flattenArrayHelper([...first, ...rest], output);
+    } else {
+        return () => flattenArrayHelper(rest, [...output, first]);
+    }
+}
+
+
+// Test-----------------
+let nestedArray = [1, [2, [3, [4, 5]], 6], 7];
+
+console.log(trampoline(flattenArrayHelper(nestedArray, output = [])));
